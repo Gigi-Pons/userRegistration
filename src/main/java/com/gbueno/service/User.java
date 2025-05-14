13d13b6300d2db1b2f39ec5d235a894837a5e199
Gigi-Pons <grecia.bueno@icloud.com>
2025-05-14 12:36:49 -0600

    initial files for userRegistration program

---
+++ src/main/java/com/gbueno/service/User.java
@@ -0,0 +1,61 @@
+package com.gbueno.service;
+
+
+public class User {
+
+    //private fields
+    private Long id;
+    private String email;
+    private String name;
+    private String password;
+
+    //constructor initializes attributes
+    public User(Long id, String email, String name, String password){
+        this. id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
