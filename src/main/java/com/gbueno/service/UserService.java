e7c4cb3cf6926727e00b7e57a4f7fed8912d8570
Gigi-Pons <grecia.atx@gmail.com>
2025-05-14 13:27:59 -0600

    added notification business logic

--- src/main/java/com/gbueno/service/UserService.java
+++ src/main/java/com/gbueno/service/UserService.java
@@ -5,13 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepo;
+    private final NotificationRepository notificationRepo;
 
     //constructor
-    public UserService(UserRepository userRepo) {
+    public UserService(UserRepository userRepo, NotificationRepository notificationRepo) {
         this.userRepo = userRepo;
+        this.notificationRepo = notificationRepo;
+
     }
 
     public void registerUser(User user) {
         userRepo.save(user);
+        notificationRepo.send("Registration successful!" , user.getEmail());
     }
 }
