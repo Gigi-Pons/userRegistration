424759f20364d822b6be2780130d1c6a099c9074
Gigi-Pons <grecia.atx@gmail.com>
2025-05-14 13:23:41 -0600

    finished notificationService

--- src/main/java/com/gbueno/service/NotificationService.java
+++ src/main/java/com/gbueno/service/NotificationService.java
@@ -11,8 +11,12 @@ public class NotificationService implements NotificationRepository{
     @Value("${mail.port}")
     private String port;
 
+    //instead of sending to email for now we'll print to screen
     @Override
     public void send(String message, String email) {
-
+        System.out.println("Email: " + email);
+        System.out.println("Message: " + message);
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + port);
     }
 }
